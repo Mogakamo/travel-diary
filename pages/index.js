9568955ddea2e4b294447cbe06e22f24345bcb37
Mogakamo <com02519@uoeld.ac.ke>
2021-11-29 17:25:28 +0300

    working with nextauth

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
-import Head from 'next/head'
+import { useSession, signIn, signOut } from "next-auth"
 
-export default function Home() {
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
   return (
-    <div>
-     <h1>Travel Diary</h1> 
-    </div>
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
   )
 }
